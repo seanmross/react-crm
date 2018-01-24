6f913c2cc6d35076f7a5ffee7eac3805a7f396dd
Sean Ross <seanmross9@gmail.com>
2018-01-23 23:25:51 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id='app'>
+        hello from react.
+      </div>
+    )
+  }
+}
+export default App
