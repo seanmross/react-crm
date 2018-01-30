6b7356570e859e0bb54a3e12ed90e9514761982d
Sean Ross <seanmross9@gmail.com>
2018-01-29 21:19:44 -0500

    added button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Contact from './contact'
 import data from './data'
+import Button from 'material-ui/Button'
 
 class Layout extends Component {
+  addContact = () => {
+    console.log('clicked button')
+  }
+
   render() {
     return (
       <div>
+        <Button onClick={ this.addContact } className="button" raised color="secondary">+ Add Contact</Button>
         { data.map( contact => <Contact key={ contact.id } {...contact} />) }
       </div>
     )
